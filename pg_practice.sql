e2b198bbdd727073519c1bdd5a61f3d6d9ca3832
Nakahata1021 <j.nakahata.cingroup2012@gmail.com>
2025-01-28 16:18:48 +0900

    課題修正その２

--- pg_practice.sql
+++ pg_practice.sql
@@ -2,10 +2,12 @@
 CREATE DATABASE practice;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
-id SERIAL PRIMARY KEY,
+  CREATE TYPE gender_enum AS ENUM ('Man', 'Woman', 'Other');
+  
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL DEFAULT '' /* 氏名 */,
   age INT NOT NULL DEFAULT 0 /* 年齢 */,
-  gender TEXT CHECK (gender IN ('Man', 'Woman', 'Other')) NOT NULL DEFAULT 'Other' /* 性別 */
+  gender gender_enum NOT NULL DEFAULT 'Other' /* 性別 */
 );
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
